fe8aca8a09162a06ceeffe96444ce2b3d87e9c66
l.ansperi <desa-07@mail.ru>
2025-05-05 20:24:53 +0300

    Цена отображается с учетом валюты Р

--- app/src/main/java/ru/practicum/sprint13koh27/CatalogItemViewData.kt
+++ app/src/main/java/ru/practicum/sprint13koh27/CatalogItemViewData.kt
@@ -33,7 +33,7 @@ class CatalogItemViewHolder(
             .centerCrop()
             .into(binding.image)
         binding.title.text = viewData.item.name
-        binding.price.text = "${viewData.item.price / 100}/${viewData.item.unit}"
+        binding.price.text = "${viewData.item.price / 100}Р/${viewData.item.unit}"
 
         if (viewData.count != null) {
             if (viewData.count == 0) {
