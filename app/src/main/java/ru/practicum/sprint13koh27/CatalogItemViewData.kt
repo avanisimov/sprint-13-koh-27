6290f55207ae2044613ba8737dcbb1db726d357e
Gleb Rudko <glebrudko@inbox.ru>
2025-05-05 20:11:49 +0300

    #4 Исправил боковые пустоты у картинок в каталоге

--- app/src/main/java/ru/practicum/sprint13koh27/CatalogItemViewData.kt
+++ app/src/main/java/ru/practicum/sprint13koh27/CatalogItemViewData.kt
@@ -15,8 +15,7 @@ data class CatalogItemViewData(
 }
 
 class CatalogItemViewHolder(
-    parent: ViewGroup,
-    val binding: VCatalogItemBinding = VCatalogItemBinding.inflate(
+    parent: ViewGroup, val binding: VCatalogItemBinding = VCatalogItemBinding.inflate(
         LayoutInflater.from(
             parent.context
         ), parent, false
@@ -28,10 +27,7 @@ class CatalogItemViewHolder(
     fun bind(viewData: CatalogItemViewData) {
         binding.root
 
-        Glide
-            .with(binding.root.context)
-            .load(viewData.item.imageUrl)
-            .into(binding.image)
+        Glide.with(binding.root.context).load(viewData.item.imageUrl).centerCrop().into(binding.image)
         binding.title.text = viewData.item.name
         binding.price.text = "${viewData.item.price / 100}/${viewData.item.unit}"
 
